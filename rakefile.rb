8b06d4469f3094737b28446de778c2d47ad0129e
jmarnold <rncodepoet@gmail.com>
2012-09-01 16:38:57 -0500

    fixes build

--- rakefile.rb
+++ rakefile.rb
@@ -36,17 +36,17 @@ end
 
 desc "Opens the Serenity Jasmine Runner in interactive mode"
 task :open do
-	serenity "jasmine interactive src/serenity.txt"
+	serenity "jasmine interactive src/serenity.txt -b Firefox"
 end
 
 desc "Runs the Jasmine tests"
 task :run => [:restore_if_missing] do
-	serenity "jasmine run --timeout 60 src/serenity.txt"
+	serenity "jasmine run --timeout 60 src/serenity.txt -b Firefox"
 end
 
 desc "Runs the Jasmine tests and outputs the results for TC"
 task :ci => [:clean, :restore_if_missing] do
-    serenity "jasmine run --verbose --timeout 60 src/serenity.txt"
+    serenity "jasmine run --verbose --timeout 60 src/serenity.txt -b Firefox"
     copyOutputFiles "src/jquery.continuations/content/scripts", "jquery.continuations.*", props[:artifacts]
 end
 
