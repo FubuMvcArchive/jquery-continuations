ac5838a340706def5ba710ed957c2dbe57cb9eb6
jmarnold <rncodepoet@gmail.com>
2013-06-02 23:00:22 -0500

    Converted to fuburake

--- rakefile.rb
+++ rakefile.rb
@@ -1,37 +1,20 @@
-COMPILE_TARGET = ENV['config'].nil? ? "debug" : ENV['config']
+require 'bundler/setup'
+  require 'fuburake'
 
-buildsupportfiles = Dir["#{File.dirname(__FILE__)}/buildsupport/*.rb"]
+@solution = FubuRake::Solution.new do |sln|
+	sln.compile = {
+		:solutionfile => 'src/jquery.continuations.sln'
+	}
 
-if( ! buildsupportfiles.any? )
-  # no buildsupport, let's go get it for them.
-  sh 'git submodule update --init' unless buildsupportfiles.any?
-  buildsupportfiles = Dir["#{File.dirname(__FILE__)}/buildsupport/*.rb"]
-end
+	sln.assembly_info = {
+		:product_name => "jQuery.continuations",
+		:copyright => 'Copyright 2008-2013 Jeremy D. Miller, Josh Arnold, et al. All rights reserved.'
+	}
 
-# nope, we still don't have buildsupport. Something went wrong.
-raise "Run `git submodule update --init` to populate your buildsupport folder." unless buildsupportfiles.any?
-
-include FileTest
-require 'albacore'
-
-PRODUCT = "jquery.continuations"
-COPYRIGHT = 'Copyright 2012 Joshua Arnold. All rights reserved.';
-
-buildsupportfiles = Dir["#{File.dirname(__FILE__)}/buildsupport/*.rb"]
-raise "Run `git submodule update --init` to populate your buildsupport folder." unless buildsupportfiles.any?
-buildsupportfiles.each { |ext| load ext }
-
-props = { :artifacts => File.expand_path("artifacts") }
-
-desc "**Default**"
-task :default => [:run]
-
-desc "Prepares the working directory for a new build"
-task :clean => [:update_buildsupport] do
-	FileUtils.rm_rf props[:artifacts]
-    # work around nasty latency issue where folder still exists for a short while after it is removed
-    waitfor { !exists?(props[:artifacts]) }
-	Dir.mkdir props[:artifacts]
+	sln.ripple_enabled = true
+	sln.fubudocs_enabled = true
+    
+    sln.ci_steps = ["run_phantom"]
 end
 
 desc "Opens the Serenity Jasmine Runner in interactive mode"
@@ -41,16 +24,17 @@ task :open do
 end
 
 desc "Runs the Jasmine tests"
-task :run => [:restore_if_missing] do
+task :run => [:compile] do
     prep()
 	serenity "jasmine run --timeout 60 src/serenity.txt -b Firefox"
 end
 
 desc "Runs the Jasmine tests and outputs the results for TC"
-task :ci => [:clean, :restore_if_missing] do
+task :run_phantom => [:compile] do
     prep()
     serenity "jasmine run --verbose --timeout 60 src/serenity.txt -b Phantom"
-    copyOutputFiles "src/jquery.continuations/content/scripts", "jquery.continuations.*", props[:artifacts]
+    artifacts = File.expand_path('artifacts', File.dirname(__FILE__))
+    copyOutputFiles "src/jquery.continuations/content/scripts", "jquery.continuations.*", artifacts
 end
 
 def prep()
@@ -60,21 +44,6 @@ def prep()
 end
 
 def self.serenity(args)
-  serenity = Platform.runtime(Nuget.tool("Serenity", "SerenityRunner.exe"))
+  serenity = Platform.runtime(Nuget.tool("Serenity", "SerenityRunner.exe"), "v4.0.30319")
   sh "#{serenity} #{args}"
-end
-
-def copyOutputFiles(fromDir, filePattern, outDir)
-  Dir.glob(File.join(fromDir, filePattern)){|file| 		
-	copy(file, outDir, :preserve => true) if File.file?(file)
-  } 
-end
-
-def waitfor(&block)
-  checks = 0
-  until block.call || checks >10 
-    sleep 0.5
-    checks += 1
-  end
-  raise 'waitfor timeout expired' if checks > 10
 end
